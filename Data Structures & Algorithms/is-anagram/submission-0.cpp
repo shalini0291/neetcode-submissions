6649fbe138632345aa862699927584efb97f7123
shalini0291 <83516259+shalini0291@users.noreply.github.com>
2026-04-17 22:28:28 -0500

    Bulk sync: 6 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!=t.size())
+            return false;
+        unordered_map<char,int> s_map;
+        unordered_map<char,int> t_map;
+
+        for(int i=0;i<s.size();i++)
+        {
+            s_map[s[i]]++;
+            t_map[t[i]]++;
+        }
+
+        for(int i=0;i<s_map.size();i++)
+        {
+            if(s_map[i]!=t_map[i])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
